e1151c5190d2c25a4ffe2085a6a819eb5d35b22f
kumabiko <nousbear.dev@gmail.com>
2023-03-05 12:47:05 +0900

    ✨ App Directoryのヘッダ、レイアウト、ページファイルの作成

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import "../styles/globals.css";
+import Navigation from "./components/navigation";
+
+// レイアウト
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html>
+      <body>
+        <div className="flex flex-col min-h-screen">
+          <Navigation />
+
+          <main className="flex-1 container max-w-screen-xl mx-auto px-5 py-10">
+            {children}
+          </main>
+
+          <footer className="py-5 border-t">
+            <div className="text-center text-sm text-gray-500">
+              Copyright © All rights reserved | Blog
+            </div>
+          </footer>
+        </div>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
